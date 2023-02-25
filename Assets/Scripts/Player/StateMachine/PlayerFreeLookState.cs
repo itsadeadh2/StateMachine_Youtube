aff9ab2366c11aa76ce07ebe7ed2103364340105
itsadeadh2 <itsadeadh2@gmail.com>
2023-02-25 11:34:47 -0400

    create jump state

--- Assets/Scripts/Player/StateMachine/PlayerFreeLookState.cs
+++ Assets/Scripts/Player/StateMachine/PlayerFreeLookState.cs
@@ -1,35 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerFreeLookState : State
-{
+public class PlayerFreeLookState : State {
     private PlayerStateMachine stateMachine;
 
     public PlayerFreeLookState(PlayerStateMachine stateMachine) {
         this.stateMachine = stateMachine;
     }
-    
-    public override void OnEnter()
-    {
+
+    public override void OnEnter() {
         Debug.Log("Entered");
+        stateMachine.InputReader.JumpEvent += OnJumpPressed;
     }
 
-    public override void OnUpdate(float deltaTime)
-    {
+    public override void OnExit() {
+        Debug.Log("Exited");
+    }
+
+    public override void OnUpdate(float deltaTime) {
         Debug.Log("Updating");
 
+        Vector3 motion = CalculateMovement();
+
+        stateMachine.Controller.Move(motion * deltaTime);
+    }
+
+    private Vector3 CalculateMovement() {
         Vector3 motion = Vector3.zero;
         motion.x = stateMachine.InputReader.MovementValue.x;
         motion.z = stateMachine.InputReader.MovementValue.y;
         motion *= stateMachine.MoveSpeed;
-
-        stateMachine.Controller.Move(motion * deltaTime);
-
+        return motion;
     }
 
-    public override void OnExit()
-    {
-        Debug.Log("Exited");
+    private void OnJumpPressed() {
+        stateMachine.switchstate 
     }
 }
---
+++ Assets/Scripts/Player/StateMachine/PlayerJumpState.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumpState : State {
+    public override void OnEnter() {
+        
+    }
+
+    public override void OnUpdate(float deltaTime) {
+        
+    }
+
+    public override void OnExit() {
+        
+    }
+}
